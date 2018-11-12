94bb3df38b7429eebefdef9a3e1fe3965e6df359
Vi Vu <Vi Vu@DESKTOP-CCFAEH9>
2018-11-12 10:48:07 +0700

    Code của con vi bê đê

--- DuAn_TestGitHub/TestAnime/Program.cs
+++ DuAn_TestGitHub/TestAnime/Program.cs
@@ -17,6 +17,7 @@ namespace TestAnime
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            ////Con vi bánh bèo
         }
     }
 }
