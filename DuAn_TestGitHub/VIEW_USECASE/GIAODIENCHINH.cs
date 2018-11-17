26ac75664ec778891d9ccf6405d96cd6270cfd19
ChieuChieu <ChieuChieu@DESKTOP-7TFB1BS>
2018-11-17 19:00:31 +0700

    Đẩy lên project VIEW_USECASE

---
+++ DuAn_TestGitHub/VIEW_USECASE/GIAODIENCHINH.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VIEW_USECASE
+{
+    public partial class GIAODIENCHINH : Form
+    {
+        public GIAODIENCHINH()
+        {
+            InitializeComponent();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
